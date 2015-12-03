af801c1e8462ab23371d763729fdd47ab0c164d8
Renaud COSTA <renaud.costa84@gmail.com>
2015-12-03 17:31:50 +0100

    Classes de maladies

--- src/main/java/sicknesses/H5N1.java
+++ src/main/java/sicknesses/H5N1.java
@@ -7,8 +7,8 @@ public class H5N1 extends Sickness {
     public H5N1()
     {
         this.incubationTime = 3;
-        this.contagionTime = 4;
-        this.recoverTime = 4;
+        this.contagionTime = 6;
+        this.recoverTime = 2;
         this.severity = 1.5;
     }
 }
